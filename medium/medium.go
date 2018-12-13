12da8591e4710dba4d264d1397db1b4c08c8048e
dongbo <dongbo@qingsongchou.com>
2018-12-13 10:54:21 +0800

    CPL33
    CPL33:
    1. 完成#33题目, 在时间复杂度 O(logN) 限制下，在翻转有序数组中找出执行值的索引。

--- medium/medium.go
+++ medium/medium.go
@@ -6,11 +6,62 @@ func Search(nums []int, target int) int {
 	if n == 0 {
 		return -1
 	}
-	k := n / 2
+	i1 := 0
+	i2 := n - 1 // 指向最小的元素
+	im := 0
 	// 找到第一个小于前一个数且小于后一个数的索引
+	/**
+	0 1 2 3 4 5
+	3 4 5 0 1 2
+	1 2 3 4 5 0
+	*/
+	for nums[i1] > nums[i2] {
+		if i2-i1 == 1 {
+			im = i2
+			break
+		}
+		im = (i1 + i2) / 2
+		if nums[im] >= nums[i1] {
+			i1 = im
+		} else if nums[im] <= nums[i2] {
+			i2 = im
+		}
+	}
 
+	if im == 0 {
+		return BinSearch(nums, target)
+	} else {
+		if nums[im] == target {
+			return im
+		} else if nums[im] > target {
+			return -1
+		}
+		nums1 := nums[:im]
+		nums2 := nums[im:]
+		if r := BinSearch(nums1, target); r != -1 {
+			return r
+		}
+		if r := BinSearch(nums2, target); r != -1 {
+			return im + r
+		}
+	}
+
+	return -1
 }
 
-func binSeach(nums []int, target int) int {
-
+func BinSearch(nums []int, target int) int {
+	var mid int
+	end := len(nums) - 1
+	start := 0
+	for end >= start {
+		mid = (start + end) / 2
+		if nums[mid] > target {
+			end = mid - 1
+		} else if nums[mid] < target {
+			start = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
 }
--- medium/medium_test.go
+++ medium/medium_test.go
@@ -1,1 +1,21 @@
 package medium
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBinSearch(t *testing.T) {
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println(BinSearch(nums, 3))
+	fmt.Println(BinSearch(nums, 6))
+}
+
+func TestSearch(t *testing.T) {
+	//nums := []int{3, 4, 5, 1, 2}
+	//	//fmt.Println(Search(nums, 4))
+	//	//fmt.Println(Search(nums, 2))
+	//	//fmt.Println(Search(nums, 6))
+	nums1 := []int{0}
+	fmt.Println(Search(nums1, 1))
+}
